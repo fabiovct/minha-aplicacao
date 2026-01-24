2407a860132d80f5124b5d61c5f37dde310fca06
Fabio Vicentini <vct.fabio.vct@gmail.com>
2026-01-24 12:35:00 -0300

    adionando documentacao scribe

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/docs', function () {
+    return view('scribe.index');
+});
+
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login.post');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
